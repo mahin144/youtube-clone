7ad2f41bde488bfc84694954371fc6dfa00d44ec
mahin144 <mahinibrahim144@gmail.com>
2023-05-09 00:37:52 -0700

    channel logo

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,8 +1,7 @@
 import { Stack } from "@mui/material";
 import { categories } from "../utils/constants";
-const selectedCategory = "New";
 
-const SideBar = () => (
+const SideBar = ({ selectedCategory, setSelectedCategory }) => (
   <Stack
     direcrion="row"
     sx={{
@@ -14,14 +13,26 @@ const SideBar = () => (
     {categories.map((category) => (
       <button
         className="category-btn"
+        onClick={() => setSelectedCategory(category.name)}
         style={{
           background: category.name === selectedCategory && "#FC1503",
           color: "white",
         }}
         key={category.name}
       >
-        <span>{category.icon}</span>
-        <span>{category.name}</span>
+        <span
+          style={{
+            color: category.name === selectedCategory ? "white" : "red",
+            marginRight: "15px",
+          }}
+        >
+          {category.icon}
+        </span>
+        <span
+          style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}
+        >
+          {category.name}
+        </span>
       </button>
     ))}
   </Stack>

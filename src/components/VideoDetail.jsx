1cab5957cadfec99f66c25384b97f2c9dcf015c8
mahin144 <mahinibrahim144@gmail.com>
2023-05-09 19:16:02 -0700

    clone youtube done

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -4,16 +4,21 @@ import ReactPlayer from "react-player";
 import { Typography, Box, Stack } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
 
-import { Video } from "./";
+import { Videos } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
       setVideoDetail(data.items[0])
+    );
+
+    fetchFromAPI(`search?part=snippet&relatedToVideos=${id}&type=video`).then(
+      (data) => setVideos(data.items)
     );
   }, [id]);
 
@@ -64,6 +69,14 @@ const VideoDetail = () => {
               </Stack>
             </Stack>
           </Box>
+        </Box>
+        <Box
+          px={2}
+          py={{ md: 1, xs: 5 }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Videos videos={videos} direction="column" />
         </Box>
       </Stack>
     </Box>
